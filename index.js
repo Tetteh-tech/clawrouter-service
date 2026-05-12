58a6922dff1e189d96d0fe12b0d6d1b5dfb4fbc9
Tetteh-tech <ocquayedavid38@gmail.com>
2026-05-12 16:48:02 +0100

    franklin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const PORT = process.env.PORT || 10000;
 
 // Start ClawRouter process
-const clawrouter = spawn('npx', ['@blockrun/clawrouter', 'start'], {
+const clawrouter = spawn('npx', ['@blockrun/franklin', 'start'], {
   stdio: 'pipe',
   shell: true
 });
